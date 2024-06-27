4613f2cabc0b86b3b73d38764fe8e9d97c4e1196
akarinDesktop <791972041@qq.com>
2024-06-27 16:33:03 +0000

    6.27

--- src/primer/trie.cpp
+++ src/primer/trie.cpp
@@ -8,7 +8,7 @@ template <class T>
 auto Trie::Get(std::string_view key) const -> const T * {
   auto node = root_;
   for (auto ch: key) {
-    if (node == nullptr || node->children_.find(ch) == node->children_.end()) {
+    if (node == nullptr || !node->children_.count(ch)) {
       return nullptr;
     }
     node = node->children_.at(ch);
@@ -18,6 +18,7 @@ auto Trie::Get(std::string_view key) const -> const T * {
     return nullptr;
   }
   return value_node->value_.get();
+
   // throw NotImplementedException("Trie::Get is not implemented.");
   // You should walk through the trie to find the corresponding to the key. If the node doesn't exist, return
   // nullptr. After you find the node, you should use `dynamic_cast` to cast it to `const TrieNodeWithValue<T> *`. If
@@ -27,28 +28,80 @@ auto Trie::Get(std::string_view key) const -> const T * {
 
 template <class T>
 auto Trie::Put(std::string_view key, T value) const -> Trie {
-  auto newRoot = std::make_shared<TrieNode>(*root_);
-  auto node = newRoot;
-  auto parent = newRoot;
-  for (auto ch: key) {
-    auto it = node->children_.find(ch);
-    if (node != newRoot) {
-      parent = node;
-    }
-    if (it == node->children_.end()) {
-      auto newNode = std::make_shared<TrieNode>();
-      node->children_[ch] = newNode;
-      node = newNode;
+  // std::cout << 1;
+  std::shared_ptr<T> val_p = std::make_shared<T>(std::move(value));
+  // std::shared_ptr<T> val_p = nullptr;
+  if (key.size() == 0) {
+    std::shared_ptr<bustub::TrieNodeWithValue<T>> newRoot = nullptr;
+    // 如果根节点无子节点
+    if (root_ == nullptr || root_->children_.empty()) {
+      // 直接修改根节点
+      newRoot = std::make_unique<TrieNodeWithValue<T>>(std::move(val_p));
     } else {
-      auto newNode = std::make_shared<TrieNode>(*it->second);
-      node->children_[ch] = newNode;
-      node = newNode;
+      // 如果有，构造一个新节点，children指向root_的children
+      newRoot = std::make_unique<TrieNodeWithValue<T>>(root_->children_, std::move(val_p));
+    }
+    // 返回新的Trie
+    return Trie(std::move(newRoot));
+  }
+  std::shared_ptr<TrieNode> newRoot = nullptr;
+  if (root_ == nullptr) {
+    newRoot = std::make_shared<TrieNode>();
+  } else {
+    newRoot = root_->Clone();
+    // newRoot = root_->Clone();
+  }
+  auto node = newRoot;
+  for (size_t i = 0; i < key.size(); ++i) {
+    if (i == key.size() - 1) {
+      if (node->children_.count(key[i])) {
+        auto nextNode = node->children_[key[i]];
+        auto newNode = std::make_shared<TrieNodeWithValue<T>>(nextNode->children_, std::move(val_p));
+        node->children_[key[i]] = newNode;
+      } else {
+        auto newNode = std::make_shared<TrieNodeWithValue<T>>(std::move(val_p));
+        node->children_[key[i]] = newNode;
+      }
+    } else {
+      if (node->children_.count(key[i])) {
+        auto nextNode = node->children_[key[i]];
+        if (nextNode->is_value_node_) {
+          auto newNode = std::make_shared<TrieNodeWithValue<T>>(std::move(nextNode));
+          node->children_[key[i]] = newNode;
+          node = newNode;
+        } else {
+          auto newNode = std::make_shared<TrieNode>(std::move(nextNode));
+          node->children_[key[i]] = newNode;
+          node = newNode;
+        }
+        // auto newNode = std::make_shared<TrieNode>(std::move(node->children_[key[i]]));
+        // node->children_[key[i]] = 
+      } else {
+        auto newNode = std::make_shared<TrieNode>();
+        node->children_[key[i]] = newNode;
+        node = newNode;
+      }
     }
   }
-  auto newValueNode = std::make_shared<TrieNodeWithValue<T>>(
-    node->children_, std::make_shared<T>(std::move(value)));
-  parent->children_[key.back()] = newValueNode;
-  return Trie(newRoot);
+  // for (auto ch: key) {
+  //   auto it = node->children_.find(ch);
+  //   if (node != newRoot) {
+  //     parent = node;
+  //   }
+  //   if (it == node->children_.end()) {
+  //     auto newNode = std::make_shared<TrieNode>();
+  //     node->children_[ch] = newNode;
+  //     node = newNode;
+  //   } else {
+  //     auto newNode = it->second->Clone();
+  //     // std::make_shared<TrieNode>(*it->second);
+  //     node->children_[ch] = newNode;
+  //     node = newNode;
+  //   }
+  // }
+  // auto newValueNode = std::make_shared<TrieNodeWithValue<T>>(node->children_, val_p);
+  // parent->children_[key.back()] = newValueNode;
+  return Trie(std::move(newRoot));
 
   // Note that `T` might be a non-copyable type. Always use `std::move` when creating `shared_ptr` on that value.
   // throw NotImplementedException("Trie::Put is not implemented.");
