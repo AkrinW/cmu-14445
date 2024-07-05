feb0b91f63a2ee49deff65b26f60ef4f7a84ad36
akarinDesktop <791972041@qq.com>
2024-07-05 15:54:03 +0000

    lruk repair

--- src/buffer/lru_k_replacer.cpp
+++ src/buffer/lru_k_replacer.cpp
@@ -73,6 +73,40 @@ auto LRUKReplacer::Evict(frame_id_t *frame_id) -> bool {
   return true;
 }
 
+// void LRUKReplacer::RecordAccess(frame_id_t frame_id, [[maybe_unused]] AccessType access_type) {
+//   std::lock_guard<std::mutex> lock(latch_);
+//   if (frame_id > static_cast<int>(replacer_size_)) {
+//     throw std::runtime_error("invalid frame_id");
+//   }
+//   if (node_store_.count(frame_id) == 0) {
+//     auto *p = new LRUKNode(frame_id, k_);
+//     node_store_.emplace(frame_id, p);
+//   }
+//   auto p = node_store_[frame_id];
+//   p->PushRecord(current_timestamp_);
+//   ++current_timestamp_;
+//   if (p->is_evictable_) {
+//     p->last_->next_ = p->next_;
+//     p->next_->last_ = p->last_;
+//     if (p->history_.size() < k_) {
+//       p->next_ = end_;
+//       p->last_ = end_->last_;
+//       end_->last_->next_ = p;
+//       end_->last_ = p;
+//     } else {
+//       auto num = p->history_.front();
+//       LRUKNode *q = head_k_->next_;
+//       while (!q->history_.empty() && q->history_.front() < num) {
+//         q = q->next_;
+//       }
+//       p->next_ = q;
+//       p->last_ = q->last_;
+//       q->last_->next_ = p;
+//       q->last_ = p;
+//     }
+//   }
+// }
+
 void LRUKReplacer::RecordAccess(frame_id_t frame_id, [[maybe_unused]] AccessType access_type) {
   std::lock_guard<std::mutex> lock(latch_);
   if (frame_id > static_cast<int>(replacer_size_)) {
@@ -88,24 +122,63 @@ void LRUKReplacer::RecordAccess(frame_id_t frame_id, [[maybe_unused]] AccessType
   if (p->is_evictable_) {
     p->last_->next_ = p->next_;
     p->next_->last_ = p->last_;
+    LRUKNode *q;
+    auto num = p->history_.front();
+
     if (p->history_.size() < k_) {
-      p->next_ = end_;
-      p->last_ = end_->last_;
-      end_->last_->next_ = p;
-      end_->last_ = p;
+      q = head_->next_;
     } else {
-      auto num = p->history_.front();
-      LRUKNode *q = head_k_->next_;
-      while (!q->history_.empty() && q->history_.front() < num) {
-        q = q->next_;
-      }
-      p->next_ = q;
-      p->last_ = q->last_;
-      q->last_->next_ = p;
-      q->last_ = p;
+      q = head_k_->next_;
     }
+    while (!q->history_.empty() && q->history_.front() < num) {
+      q = q->next_;
+    }
+    p->next_ = q;
+    p->last_ = q->last_;
+    q->last_->next_ = p;
+    q->last_ = p;
   }
 }
+
+// void LRUKReplacer::SetEvictable(frame_id_t frame_id, bool set_evictable) {
+//   std::lock_guard<std::mutex> lock(latch_);
+//   if (node_store_.count(frame_id) == 0 || frame_id > static_cast<int>(replacer_size_)) {
+//     throw std::runtime_error("invalid frame_id");
+//   }
+//   if (node_store_.at(frame_id)->is_evictable_ == set_evictable) {
+//     return;
+//   }
+//   LRUKNode *p = node_store_.at(frame_id);
+//   p->is_evictable_ = set_evictable;
+//   if (set_evictable) {
+//     ++curr_size_;
+//     if (p->history_.size() == k_) {
+//       auto num = p->history_.front();
+//       LRUKNode *q = head_k_->next_;
+//       while (!q->history_.empty() && q->history_.front() < num) {
+//         q = q->next_;
+//       }
+//       p->next_ = q;
+//       p->last_ = q->last_;
+//       q->last_->next_ = p;
+//       q->last_ = p;
+//     } else {
+//       auto num = p->history_.back();
+//       LRUKNode *q = end_->last_;
+//       while (!q->history_.empty() && q->history_.back() > num) {
+//         q = q->last_;
+//       }
+//       p->last_ = q;
+//       p->next_ = q->next_;
+//       q->next_->last_ = p;
+//       q->next_ = p;
+//     }
+//   } else {
+//     --curr_size_;
+//     p->last_->next_ = p->next_;
+//     p->next_->last_ = p->last_;
+//   }
+// }
 
 void LRUKReplacer::SetEvictable(frame_id_t frame_id, bool set_evictable) {
   std::lock_guard<std::mutex> lock(latch_);
@@ -119,27 +192,20 @@ void LRUKReplacer::SetEvictable(frame_id_t frame_id, bool set_evictable) {
   p->is_evictable_ = set_evictable;
   if (set_evictable) {
     ++curr_size_;
+    auto num = p->history_.front();
+    LRUKNode *q;
     if (p->history_.size() == k_) {
-      auto num = p->history_.front();
-      LRUKNode *q = head_k_->next_;
-      while (!q->history_.empty() && q->history_.front() < num) {
-        q = q->next_;
-      }
-      p->next_ = q;
-      p->last_ = q->last_;
-      q->last_->next_ = p;
-      q->last_ = p;
+      q = head_k_->next_;
     } else {
-      auto num = p->history_.back();
-      LRUKNode *q = end_->last_;
-      while (!q->history_.empty() && q->history_.back() > num) {
-        q = q->last_;
-      }
-      p->last_ = q;
-      p->next_ = q->next_;
-      q->next_->last_ = p;
-      q->next_ = p;
+      q = head_->next_;
     }
+    while (!q->history_.empty() && q->history_.front() < num) {
+      q = q->next_;
+    }
+    p->next_ = q;
+    p->last_ = q->last_;
+    q->last_->next_ = p;
+    q->last_ = p;
   } else {
     --curr_size_;
     p->last_->next_ = p->next_;
