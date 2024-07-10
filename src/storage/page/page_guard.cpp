01308c4d25754c94a33cf820d1c99151b08f6669
akarinDesktop <791972041@qq.com>
2024-07-10 17:00:49 +0000

    page_guard

--- src/storage/page/page_guard.cpp
+++ src/storage/page/page_guard.cpp
@@ -3,13 +3,56 @@
 
 namespace bustub {
 
-BasicPageGuard::BasicPageGuard(BasicPageGuard &&that) noexcept {}
+BasicPageGuard::BasicPageGuard(BasicPageGuard &&that) noexcept {
+    bpm_ = that.bpm_;
+    page_ = that.page_;
+    is_dirty_ = that.is_dirty_;
+    that.bpm_ = nullptr;
+    that.page_ = nullptr;
+}
 
-void BasicPageGuard::Drop() {}
+void BasicPageGuard::Drop() {
+    if (bpm_ != nullptr && page_ != nullptr) {
+        bpm_->UnpinPage(page_->GetPageId(), is_dirty_);
+    }
+    bpm_ = nullptr;
+    page_ = nullptr;
+}
 
-auto BasicPageGuard::operator=(BasicPageGuard &&that) noexcept -> BasicPageGuard & { return *this; }
+auto BasicPageGuard::operator=(BasicPageGuard &&that) noexcept -> BasicPageGuard & { 
+    Drop();
+    bpm_ = that.bpm_;
+    page_ = that.page_;
+    is_dirty_ = that.is_dirty_;
+    that.bpm_ = nullptr;
+    that.page_ = nullptr;
+    return *this; 
+}
 
-BasicPageGuard::~BasicPageGuard(){};  // NOLINT
+BasicPageGuard::~BasicPageGuard(){
+    Drop();
+};  // NOLINT
+
+auto BasicPageGuard::UpgradeRead()->ReadPageGuard {
+    if (page_ != nullptr) {
+        page_->RLatch();
+    }
+    auto read_page_guard = ReadPageGuard(bpm_, page_);
+    bpm_ = nullptr;
+    page_ = nullptr;
+    return read_page_guard;
+}
+
+auto BasicPageGuard::UpgradeWrite()->WritePageGuard {
+    if (page_ != nullptr) {
+        page_->WLatch();
+    }
+    auto write_page_guard = WritePageGuard(bpm_, page_);
+    bpm_ = nullptr;
+    page_ = nullptr;
+    return write_page_guard;
+}
+
 
 ReadPageGuard::ReadPageGuard(ReadPageGuard &&that) noexcept = default;
 
