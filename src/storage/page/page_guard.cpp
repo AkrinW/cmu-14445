4db9df30f174f062d487a82a5b656c8118863f08
akarinDesktop <791972041@qq.com>
2024-07-11 04:19:24 +0000

    page_guard

--- src/storage/page/page_guard.cpp
+++ src/storage/page/page_guard.cpp
@@ -4,70 +4,85 @@
 namespace bustub {
 
 BasicPageGuard::BasicPageGuard(BasicPageGuard &&that) noexcept {
-    bpm_ = that.bpm_;
-    page_ = that.page_;
-    is_dirty_ = that.is_dirty_;
-    that.bpm_ = nullptr;
-    that.page_ = nullptr;
+  bpm_ = that.bpm_;
+  page_ = that.page_;
+  is_dirty_ = that.is_dirty_;
+  that.bpm_ = nullptr;
+  that.page_ = nullptr;
 }
 
 void BasicPageGuard::Drop() {
-    if (bpm_ != nullptr && page_ != nullptr) {
-        bpm_->UnpinPage(page_->GetPageId(), is_dirty_);
-    }
-    bpm_ = nullptr;
-    page_ = nullptr;
+  if (bpm_ != nullptr && page_ != nullptr) {
+    bpm_->UnpinPage(page_->GetPageId(), is_dirty_);
+  }
+  bpm_ = nullptr;
+  page_ = nullptr;
 }
 
-auto BasicPageGuard::operator=(BasicPageGuard &&that) noexcept -> BasicPageGuard & { 
-    Drop();
-    bpm_ = that.bpm_;
-    page_ = that.page_;
-    is_dirty_ = that.is_dirty_;
-    that.bpm_ = nullptr;
-    that.page_ = nullptr;
-    return *this; 
+auto BasicPageGuard::operator=(BasicPageGuard &&that) noexcept -> BasicPageGuard & {
+  Drop();
+  bpm_ = that.bpm_;
+  page_ = that.page_;
+  is_dirty_ = that.is_dirty_;
+  that.bpm_ = nullptr;
+  that.page_ = nullptr;
+  return *this;
 }
 
-BasicPageGuard::~BasicPageGuard(){
-    Drop();
-};  // NOLINT
+BasicPageGuard::~BasicPageGuard() { Drop(); };  // NOLINT
 
-auto BasicPageGuard::UpgradeRead()->ReadPageGuard {
-    if (page_ != nullptr) {
-        page_->RLatch();
-    }
-    auto read_page_guard = ReadPageGuard(bpm_, page_);
-    bpm_ = nullptr;
-    page_ = nullptr;
-    return read_page_guard;
+auto BasicPageGuard::UpgradeRead() -> ReadPageGuard {
+  if (page_ != nullptr) {
+    page_->RLatch();
+  }
+  auto read_page_guard = ReadPageGuard(bpm_, page_);
+  bpm_ = nullptr;
+  page_ = nullptr;
+  return read_page_guard;
 }
 
-auto BasicPageGuard::UpgradeWrite()->WritePageGuard {
-    if (page_ != nullptr) {
-        page_->WLatch();
-    }
-    auto write_page_guard = WritePageGuard(bpm_, page_);
-    bpm_ = nullptr;
-    page_ = nullptr;
-    return write_page_guard;
+auto BasicPageGuard::UpgradeWrite() -> WritePageGuard {
+  if (page_ != nullptr) {
+    page_->WLatch();
+  }
+  auto write_page_guard = WritePageGuard(bpm_, page_);
+  bpm_ = nullptr;
+  page_ = nullptr;
+  return write_page_guard;
 }
 
+ReadPageGuard::ReadPageGuard(ReadPageGuard &&that) noexcept { guard_ = std::move(that.guard_); }
 
-ReadPageGuard::ReadPageGuard(ReadPageGuard &&that) noexcept = default;
+auto ReadPageGuard::operator=(ReadPageGuard &&that) noexcept -> ReadPageGuard & {
+  Drop();
+  guard_ = std::move(that.guard_);
+  return *this;
+}
 
-auto ReadPageGuard::operator=(ReadPageGuard &&that) noexcept -> ReadPageGuard & { return *this; }
+void ReadPageGuard::Drop() {
+  if (guard_.page_ != nullptr) {
+    guard_.page_->RUnlatch();  // 这里的释放锁和升级成readguard时获取的锁对应了。
+  }
+  guard_.Drop();
+}
 
-void ReadPageGuard::Drop() {}
+ReadPageGuard::~ReadPageGuard() { Drop(); }  // NOLINT
 
-ReadPageGuard::~ReadPageGuard() {}  // NOLINT
+WritePageGuard::WritePageGuard(WritePageGuard &&that) noexcept { guard_ = std::move(that.guard_); }
 
-WritePageGuard::WritePageGuard(WritePageGuard &&that) noexcept = default;
+auto WritePageGuard::operator=(WritePageGuard &&that) noexcept -> WritePageGuard & {
+  Drop();
+  guard_ = std::move(that.guard_);
+  return *this;
+}
 
-auto WritePageGuard::operator=(WritePageGuard &&that) noexcept -> WritePageGuard & { return *this; }
+void WritePageGuard::Drop() {
+  if (guard_.page_ != nullptr) {
+    guard_.page_->WUnlatch();
+  }
+  guard_.Drop();
+}
 
-void WritePageGuard::Drop() {}
-
-WritePageGuard::~WritePageGuard() {}  // NOLINT
+WritePageGuard::~WritePageGuard() { Drop(); }  // NOLINT
 
 }  // namespace bustub
