26faa2ed0dd515514d42b05d17131f7799063fa9
akarinDesktop <791972041@qq.com>
2024-07-20 17:00:29 +0000

    index

--- src/execution/index_scan_executor.cpp
+++ src/execution/index_scan_executor.cpp
@@ -13,10 +13,58 @@
 
 namespace bustub {
 IndexScanExecutor::IndexScanExecutor(ExecutorContext *exec_ctx, const IndexScanPlanNode *plan)
-    : AbstractExecutor(exec_ctx) {}
+    : AbstractExecutor(exec_ctx), plan_(plan) {}
 
-void IndexScanExecutor::Init() { throw NotImplementedException("IndexScanExecutor is not implemented"); }
+void IndexScanExecutor::Init() { 
+    // throw NotImplementedException("IndexScanExecutor is not implemented"); 
+    //获取表信息table_info, schema, index
+    auto table_oid = plan_->table_oid_;
+    auto index_oid = plan_->GetIndexOid();
+    auto catalog = exec_ctx_->GetCatalog();
+    auto table_info = catalog->GetTable(table_oid);
+    auto index_info = catalog->GetIndex(index_oid);
+    table_heap_ = table_info->table_.get();
 
-auto IndexScanExecutor::Next(Tuple *tuple, RID *rid) -> bool { return false; }
+    htable_ = dynamic_cast<HashTableIndexForTwoIntegerColumn*>(index_info->index_.get());
+    auto index_schema = index_info->key_schema_;
+    // 获取键值
+    auto key = plan_->pred_key_;
+    auto value = key->val_;
+    std::vector<Value> values{value};
+    // 用索引的键模式创建键
+    Tuple index_key{values, &index_schema};
+    result_rid_.clear();
+    htable_->ScanKey(index_key, &result_rid_, exec_ctx_->GetTransaction());
+    }
+
+auto IndexScanExecutor::Next(Tuple *tuple, RID *rid) -> bool { 
+    //获取表信息table_info, schema, index
+    // auto table_oid = plan_->table_oid_;
+    // auto index_oid = plan_->GetIndexOid();
+    // auto catalog = exec_ctx_->GetCatalog();
+    // auto table_info = catalog->GetTable(table_oid);
+    // auto index_info = catalog->GetIndex(index_oid);
+    // auto table_schema = table_info->schema_;
+    // auto table_name = table_info->name_;
+    // auto table_heap = table_info->table_.get();
+    // auto table_indexes = catalog->GetTableIndexes(table_name);
+    // htable_ = dynamic_cast<HashTableIndexForTwoIntegerColumn*>(index_info->index_.get());
+
+    // 用索引的键模式创建键
+    // Tuple index_key{values, &index_schema};
+    // std::vector<RID> result_rid;
+    // htable_->ScanKey(index_key, &result_rid, exec_ctx_->GetTransaction());
+
+    if (result_rid_.empty()){
+return false; 
+    }
+    TupleMeta meta{};
+    meta = table_heap_->GetTuple(*result_rid_.begin()).first;
+    if (!meta.is_deleted_) {
+        *tuple = table_heap_->GetTuple(*result_rid_.begin()).second;
+        *rid = *result_rid_.begin();
+    }
+    return true;
+    }
 
 }  // namespace bustub
--- src/optimizer/seqscan_as_indexscan.cpp
+++ src/optimizer/seqscan_as_indexscan.cpp
@@ -1,11 +1,60 @@
 #include "optimizer/optimizer.h"
-
+#include "execution/plans/seq_scan_plan.h"
+#include "execution/plans/index_scan_plan.h"
+#include "execution/expressions/column_value_expression.h"
+#include "execution/expressions/comparison_expression.h"
+#include "execution/expressions/logic_expression.h"
+#include "storage/index/generic_key.h"
 namespace bustub {
 
 auto Optimizer::OptimizeSeqScanAsIndexScan(const bustub::AbstractPlanNodeRef &plan) -> AbstractPlanNodeRef {
   // TODO(student): implement seq scan with predicate -> index scan optimizer rule
   // The Filter Predicate Pushdown has been enabled for you in optimizer.cpp when forcing starter rule
-  return plan;
+  std::vector<AbstractPlanNodeRef> children;
+  for (const auto &child : plan->GetChildren()) {
+    children.emplace_back(OptimizeSeqScanAsIndexScan(child));
+  }
+
+  auto optimized_plan = plan->CloneWithChildren(std::move(children));
+  // paln计划是顺序扫描，转换成索引
+  if (optimized_plan->GetType() == PlanType::SeqScan) {
+    const auto &seq_scan_plan = dynamic_cast<const SeqScanPlanNode &>(*optimized_plan);
+    auto predicate =seq_scan_plan.filter_predicate_;
+    if (predicate != nullptr) {//如果谓词为空，需要顺序扫描，否则进一步优化
+      auto table_name = seq_scan_plan.table_name_;
+      auto table_index = catalog_.GetTableIndexes(table_name);
+      auto logic_expr = std::dynamic_pointer_cast<LogicExpression>(predicate);
+      if (!table_index.empty() && !logic_expr) {//没有索引或者有多个谓词条件，返回顺序扫描
+        auto equal_expr = std::dynamic_pointer_cast<ComparisonExpression>(predicate);
+        // 需要判断是否是条件谓词
+        if (equal_expr) {
+          auto com_type = equal_expr->comp_type_;
+          if (com_type == ComparisonType::Equal) {
+            auto table_oid = seq_scan_plan.table_oid_;
+            //返回索引表节点
+            auto column_expr = dynamic_cast<const ColumnValueExpression &>(*equal_expr->GetChildAt(0));
+            // 根据谓词的列获取表的info
+            auto column_index = column_expr.GetColIdx();
+            auto col_name = this->catalog_.GetTable(table_oid)->schema_.GetColumn(column_index).GetName();
+            //存在相关索引，获取索引info
+            for (auto *index:table_index) {
+              const auto &columns = index->index_->GetKeyAttrs();
+              std::vector<uint32_t> column_ids;
+              column_ids.push_back(column_index);
+              if (columns == column_ids) {
+                //获取predkey
+                auto pred_key = std::dynamic_pointer_cast<ConstantValueExpression>(equal_expr->GetChildAt(1));
+                //从智能指针获取裸指针
+                ConstantValueExpression *raw_pred_key = pred_key ? pred_key.get() : nullptr;
+                return std::make_shared<IndexScanPlanNode>(seq_scan_plan.output_schema_, table_oid, index->index_oid_,predicate,raw_pred_key);
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  return optimized_plan;
 }
 
 }  // namespace bustub
