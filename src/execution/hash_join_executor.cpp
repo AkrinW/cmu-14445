57071d58ac6bc466a6c137b5ff04351c462fabd4
akarinDesktop <791972041@qq.com>
2024-07-22 08:10:01 +0000

    hash_join_executor

--- src/execution/hash_join_executor.cpp
+++ src/execution/hash_join_executor.cpp
@@ -1,104 +1,119 @@
-#include "execution/executors/hash_join_executor.h"
+//===----------------------------------------------------------------------===//
+//
+//                         BusTub
+//
+// hash_join_executor.cpp
+//
+// Identification: src/execution/hash_join_executor.cpp
+//
+// Copyright (c) 2015-2021, Carnegie Mellon University Database Group
+//
+//===----------------------------------------------------------------------===//
 
+#include "execution/executors/hash_join_executor.h"
+#include "type/value_factory.h"
 namespace bustub {
 
 HashJoinExecutor::HashJoinExecutor(ExecutorContext *exec_ctx, const HashJoinPlanNode *plan,
                                    std::unique_ptr<AbstractExecutor> &&left_child,
                                    std::unique_ptr<AbstractExecutor> &&right_child)
-    : AbstractExecutor(exec_ctx) {
-  this->plan_ = plan;
-  this->left_child_ = std::move(left_child);
-  this->right_child_ = std::move(right_child);
+    : AbstractExecutor(exec_ctx), plan_(plan), left_child_(std::move(left_child)), right_child_(std::move(right_child)) {
   if (!(plan->GetJoinType() == JoinType::LEFT || plan->GetJoinType() == JoinType::INNER)) {
     // Note for 2023 Fall: You ONLY need to implement left join and inner join.
     throw bustub::NotImplementedException(fmt::format("join type {} not supported", plan->GetJoinType()));
-  }
+  } 
 }
 
-void HashJoinExecutor::Init() {
-  // 初始化左右plan的左右孩子
-  this->left_child_->Init();
-  this->right_child_->Init();
-  // 获取左执行器符合条件的元组，left_bool_用于判断左执行器是否还有符合条件的元组
+void HashJoinExecutor::Init() { 
+  // throw NotImplementedException("HashJoinExecutor is not implemented");
+  left_child_->Init();
+  right_child_->Init();
+  
   left_bool_ = left_child_->Next(&left_tuple_, &left_rid_);
-  // NEXT方法的輸出參數，用于存储查询结果
-  Tuple right_tuple{};
-  RID right_rid{};
-  //构建哈希表
   jht_ = std::make_unique<SimpleHashJoinHashTable>();
-  // 遍历子执行器，将右子执行器中的获取的数据插入到join哈希表中
   // 不能在HashJoinExecutor执行器的next中完成，因为执行器需要先从子执行器中获取所有数据，然后对这些数据进行join，最后才能产生输出结果
-  while (right_child_->Next(&right_tuple, &right_rid)) {
-    jht_->InsertKey(GetRightJoinKey(&right_tuple), right_tuple);
+  while (right_child_->Next(&right_tuple_, &right_rid_)) {
+    jht_->InsertKey(GetRightJoinKey(&right_tuple_), right_tuple_);
   }
-  // 获取左侧元组的hash key
-  auto left_hash_key = GetLeftJoinKey(&left_tuple_);
-  // 在哈希表中查找与左侧元组匹配的右侧元组
-  right_tuple_ = jht_->GetValue(left_hash_key);
-  //这里必须判断right_tuple_是否为空，否则指针会指向空地址报错
-  // 不为空说明找到了哈希值一样的
-  if (right_tuple_ != nullptr) {
-    jht_iterator_ = right_tuple_->begin();
-    // 标记为true，防止next函数中重复输出
-    has_done_ = true;
+  // 获取左tuple的key
+  auto left_key = GetLeftJoinKey(&left_tuple_);
+  right_tuple_vector_ = jht_->GetValue(left_key);
+  if (right_tuple_vector_ == nullptr) {
+    // 用于左连接没有匹配到的情况
+    if_hashjoined_ = false;
   } else {
-    // 标记为false，主要用于左连接没有匹配的情况
-    has_done_ = false;
+    iter_ = right_tuple_vector_->begin();
+    if_hashjoined_ = true;
   }
-}
+  
+   }
 
-auto HashJoinExecutor::Next(Tuple *tuple, RID *rid) -> bool {
-  // 用while的原因：如果是内连接，如果没有匹配的元组，则该轮不输出任何元组，不需要返回值，继续往下查找其他左元组
+auto HashJoinExecutor::Next(Tuple *tuple, RID *rid) -> bool { 
+  // 类似nestloopjoin的思路，循环查找左右匹配，如果内连接且不匹配，就不需要输出任何值
   while (true) {
-    // 如果right_tuple_不为空，且jht_iterator_未遍历完，则遍历输出
     // 一个左边可能匹配多个右边
-    if (right_tuple_ != nullptr && jht_iterator_ != right_tuple_->end()) {
+    if (right_tuple_vector_ != nullptr && iter_ != right_tuple_vector_->end()) {
       std::vector<Value> values;
-      auto right_tuple = *jht_iterator_;
-      for (uint32_t i = 0; i < this->left_child_->GetOutputSchema().GetColumnCount(); i++) {
-        values.emplace_back(left_tuple_.GetValue(&this->left_child_->GetOutputSchema(), i));
+      for (uint32_t i = 0; i < left_child_->GetOutputSchema().GetColumnCount(); ++i) {
+        values.emplace_back(left_tuple_.GetValue(&left_child_->GetOutputSchema(), i));
       }
-      // 连接操作右边元组的值均不为null
-      for (uint32_t i = 0; i < this->right_child_->GetOutputSchema().GetColumnCount(); i++) {
-        values.emplace_back(right_tuple.GetValue(&this->right_child_->GetOutputSchema(), i));
+      auto right_tuple = *iter_;
+      for (uint32_t i = 0; i < right_child_->GetOutputSchema().GetColumnCount(); ++i) {
+        values.emplace_back(right_tuple.GetValue(&right_child_->GetOutputSchema(), i));
       }
       *tuple = Tuple{values, &GetOutputSchema()};
-      ++jht_iterator_;
+      ++iter_;
       return true;
     }
     // 如果right_tuple_为空，或者jht_iterator_遍历完，且为左连接
     // 如果has_done_为false，则说明左连接没有匹配的元组，需要输出右元组为null的情况
-    if (plan_->GetJoinType() == JoinType::LEFT && !has_done_) {
+    if (plan_->GetJoinType() == JoinType::LEFT && !if_hashjoined_) {
       std::vector<Value> values;
-      for (uint32_t i = 0; i < this->left_child_->GetOutputSchema().GetColumnCount(); i++) {
-        values.emplace_back(left_tuple_.GetValue(&this->left_child_->GetOutputSchema(), i));
+      for(uint32_t i = 0; i < left_child_->GetOutputSchema().GetColumnCount(); ++i) {
+        values.emplace_back(left_tuple_.GetValue(&left_child_->GetOutputSchema(),i));
       }
-      // 连接操作右边元组的值均不为null
-      for (uint32_t i = 0; i < this->right_child_->GetOutputSchema().GetColumnCount(); i++) {
-        values.emplace_back(
-            ValueFactory::GetNullValueByType(this->right_child_->GetOutputSchema().GetColumn(i).GetType()));
+      // 右边元组值均为null
+      for (uint32_t i = 0; i < right_child_->GetOutputSchema().GetColumnCount(); ++i) {
+        values.emplace_back(ValueFactory::GetNullValueByType(right_child_->GetOutputSchema().GetColumn(i).GetType()));
       }
       *tuple = Tuple{values, &GetOutputSchema()};
-      has_done_ = true;
+      if_hashjoined_ = true;
       return true;
     }
     // 如果不是左连接，或者为左连接，但有有效输出，则继续遍历下一个左元组进行匹配
     // 如果left_bool_为false，左边找完了
-    left_bool_ = left_child_->Next(&this->left_tuple_, &this->left_rid_);
-    if (!left_bool_) {
+    if_hashjoined_ = left_child_->Next(&left_tuple_, &left_rid_);
+    if (!if_hashjoined_) {
       return false;
     }
-    // 重置右边匹配的元组，以及更新迭代器
-    auto left_hash_key = GetLeftJoinKey(&left_tuple_);
-    // 在哈希表中查找与左侧元组匹配的右侧元组
-    right_tuple_ = jht_->GetValue(left_hash_key);
-    if (right_tuple_ != nullptr) {
-      jht_iterator_ = right_tuple_->begin();
-      has_done_ = true;
+    // 重置右边的元组，更新迭代器
+    auto left_key = GetLeftJoinKey(&left_tuple_);
+    right_tuple_vector_ = jht_->GetValue(left_key);
+    if (right_tuple_vector_ != nullptr) {
+      iter_ = right_tuple_vector_->begin();
+      if_hashjoined_ = true;
     } else {
-      has_done_ = false;
+      if_hashjoined_ = false;
     }
   }
+  return false; 
+  }
+
+auto HashJoinExecutor::GetLeftJoinKey(const Tuple *tuple)->bustub::HashJoinKey {
+  HashJoinKey key{};
+  for (const auto &expr: plan_->LeftJoinKeyExpressions()) {
+    key.AddKey(expr->Evaluate(tuple, left_child_->GetOutputSchema()));
+  }
+  return key;
 }
+
+auto HashJoinExecutor::GetRightJoinKey(const Tuple *tuple)->bustub::HashJoinKey {
+    HashJoinKey key{};
+  for (const auto &expr: plan_->RightJoinKeyExpressions()) {
+    key.AddKey(expr->Evaluate(tuple, right_child_->GetOutputSchema()));
+  }
+  return key;
+}
+
 
 }  // namespace bustub
--- src/include/execution/executors/hash_join_executor.h
+++ src/include/execution/executors/hash_join_executor.h
@@ -1,43 +1,87 @@
+//===----------------------------------------------------------------------===//
+//
+//                         BusTub
+//
+// hash_join_executor.h
+//
+// Identification: src/include/execution/executors/hash_join_executor.h
+//
+// Copyright (c) 2015-2021, Carnegie Mellon University Database Group
+//
+//===----------------------------------------------------------------------===//
+
+#pragma once
+
 #include <memory>
 #include <utility>
-
-#include "aggregation_executor.h"
+#include "common/util/hash_util.h"
 #include "execution/executor_context.h"
 #include "execution/executors/abstract_executor.h"
 #include "execution/plans/hash_join_plan.h"
 #include "storage/table/tuple.h"
 
-namespace bustub {
 
-/** HashJoinKeyrepresents a key in an join operation */
+namespace bustub {
+  // copy from aggregate
+/** AggregateKey represents a key in an aggregation operation */
 struct HashJoinKey {
-  std::vector<Value> hash_keys_;
+  /** The group-by values */
+  std::vector<Value> group_bys_;
   /**
-   * Compares two hash joi keys for equality
-   * @param other the other hash join key to be compared with
-   * @return `true` if both hash join key have equivalent values
+   * Compares two aggregate keys for equality.
+   * @param other the other aggregate key to be compared with
+   * @return `true` if both aggregate keys have equivalent group-by expressions, `false` otherwise
    */
   auto operator==(const HashJoinKey &other) const -> bool {
-    // 比较两个对象的hash_keys_成员中的每个Value对象是否相等
-    for (uint32_t i = 0; i < other.hash_keys_.size(); ++i) {
-      if (hash_keys_[i].CompareEquals(other.hash_keys_[i]) != CmpBool::CmpTrue) {
+    for (uint32_t i = 0; i < other.group_bys_.size(); i++) {
+      if (group_bys_[i].CompareEquals(other.group_bys_[i]) != CmpBool::CmpTrue) {
         return false;
       }
     }
     return true;
   }
+  HashJoinKey() {
+    group_bys_.clear();
+  }
+  HashJoinKey(const Value & value) {
+    group_bys_.push_back(value);
+  }
+  void AddKey(const Value & value) {
+    group_bys_.push_back(value);
+  }
+  auto GetVector()->std::vector<Value> * {
+    return &group_bys_;
+  }
+};
+// copy from aggregate
+/** AggregateValue represents a value for each of the running aggregates */
+struct HashJoinValue {
+  /** The aggregate values */
+  std::vector<Tuple> hashjoins_;
+  HashJoinValue() {
+    hashjoins_.clear();
+  }
+  HashJoinValue(const Tuple &tuple) {
+    hashjoins_.push_back(tuple);
+  }
+  void AddTuple(const Tuple &tuple) {
+    hashjoins_.push_back(tuple);
+  }
+  auto GetVector() -> std::vector<Tuple>* {
+    return &hashjoins_;
+  }
 };
 }  // namespace bustub
 
 namespace std {
+
 /** Implements std::hash on AggregateKey */
 template <>
 struct hash<bustub::HashJoinKey> {
-  auto operator()(const bustub::HashJoinKey &join_key) const -> std::size_t {
+  auto operator()(const bustub::HashJoinKey &agg_key) const -> std::size_t {
     size_t curr_hash = 0;
-    for (const auto &key : join_key.hash_keys_) {
+    for (const auto &key : agg_key.group_bys_) {
       if (!key.IsNull()) {
-        // 对每一个非空的value对象，计算出它的哈希值
         curr_hash = bustub::HashUtil::CombineHashes(curr_hash, bustub::HashUtil::HashValue(&key));
       }
     }
@@ -48,38 +92,33 @@ struct hash<bustub::HashJoinKey> {
 }  // namespace std
 
 namespace bustub {
-/**
- * A simplified hash table that has all the necessary functionality for join.
- */
+
+
+
 class SimpleHashJoinHashTable {
- public:
-  /** 插入join key和tuple构建hash表 */
-  void InsertKey(const HashJoinKey &join_key, const Tuple &tuple) {
-    if (ht_.count(join_key) == 0) {
-      std::vector<Tuple> tuple_vector;
-      tuple_vector.push_back(tuple);
-      ht_.insert({join_key, tuple_vector});
-    } else {
-      ht_.at(join_key).push_back(tuple);
+  public:
+    void InsertKey(const HashJoinKey &key, const Tuple &tuple) {
+      if (htable_.count(key) == 0) {
+        htable_.insert({key, HashJoinValue(tuple)});
+      } else {
+        htable_.at(key).AddTuple(tuple);
+      }
     }
-  }
-  /** 获取该join key对应的tuple */
-  auto GetValue(const HashJoinKey &join_key) -> std::vector<Tuple> * {
-    if (ht_.find(join_key) == ht_.end()) {
-      return nullptr;
+    auto GetValue(const HashJoinKey &key) -> std::vector<Tuple> * {
+      if (htable_.find(key) == htable_.end()) {
+        return nullptr;
+      }
+      return htable_.at(key).GetVector();
     }
-    return &(ht_.find(join_key)->second);
-  }
-
-  /**
-   * Clear the hash table
-   */
-  void Clear() { ht_.clear(); }
-
- private:
-  /** The hash table is just a map from aggregate keys to aggregate values */
-  std::unordered_map<HashJoinKey, std::vector<Tuple>> ht_{};
+    void Clear() {
+      htable_.clear();
+    }
+  private:
+    std::unordered_map<HashJoinKey, HashJoinValue> htable_;
 };
+
+
+
 /**
  * HashJoinExecutor executes a nested-loop JOIN on two tables.
  */
@@ -110,38 +149,22 @@ class HashJoinExecutor : public AbstractExecutor {
   auto GetOutputSchema() const -> const Schema & override { return plan_->OutputSchema(); };
 
  private:
-  auto GetLeftJoinKey(const Tuple *tuple) -> HashJoinKey {
-    std::vector<Value> values;
-    for (const auto &expr : plan_->LeftJoinKeyExpressions()) {
-      values.emplace_back(expr->Evaluate(tuple, left_child_->GetOutputSchema()));
-    }
-    return {values};
-  }
-
-  auto GetRightJoinKey(const Tuple *tuple) -> HashJoinKey {
-    std::vector<Value> values;
-    for (const auto &expr : plan_->RightJoinKeyExpressions()) {
-      values.emplace_back(expr->Evaluate(tuple, right_child_->GetOutputSchema()));
-    }
-    return {values};
-  }
+  auto GetLeftJoinKey(const Tuple *tuple)-> HashJoinKey;
+  auto GetRightJoinKey(const Tuple *tuple)->HashJoinKey;
   /** The HashJoin plan node to be executed. */
   const HashJoinPlanNode *plan_;
-  // 遍历哈希表的迭代器
-  std::vector<Tuple>::iterator jht_iterator_;
-  // 哈希表
-  std::unique_ptr<SimpleHashJoinHashTable> jht_;
-  // 指向左表的执行器对象
   std::unique_ptr<AbstractExecutor> left_child_;
-  // 指向右表的执行器对象
   std::unique_ptr<AbstractExecutor> right_child_;
   Tuple left_tuple_{};
   RID left_rid_{};
-  std::vector<Tuple> *right_tuple_{nullptr};
-  bool has_done_{false};
-  // 用来判断左边还有没有符合要求的元组
+  Tuple right_tuple_{};
+  RID right_rid_{};
+
+  std::unique_ptr<SimpleHashJoinHashTable> jht_;
+  bool if_hashjoined_{false};
   bool left_bool_{false};
+  std::vector<Tuple>* right_tuple_vector_{nullptr};
+  std::vector<Tuple>::iterator iter_;
 };
 
 }  // namespace bustub
-
