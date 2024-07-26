86f606438fdb2aada1d80a691a9fe867ab5b8406
akarinDesktop <791972041@qq.com>
2024-07-26 06:49:31 +0000

    Reconstruct Tuple

--- src/execution/execution_common.cpp
+++ src/execution/execution_common.cpp
@@ -12,7 +12,41 @@ namespace bustub {
 
 auto ReconstructTuple(const Schema *schema, const Tuple &base_tuple, const TupleMeta &base_meta,
                       const std::vector<UndoLog> &undo_logs) -> std::optional<Tuple> {
-  UNIMPLEMENTED("not implemented");
+  // UNIMPLEMENTED("not implemented");
+  auto new_schema = schema;
+  std::vector<Value> new_values{};
+  new_values.reserve(new_schema->GetColumnCount());
+  // if (!base_meta.is_deleted_) {
+  for (uint32_t i = 0; i < new_schema->GetColumnCount(); ++i) {
+    new_values.push_back(base_tuple.GetValue(new_schema, i));
+  }
+  // }
+  bool flag = base_meta.is_deleted_;
+  auto undolog_size = undo_logs.size();
+  for (size_t i = 0; i < undolog_size; ++i) {
+    auto undolog = undo_logs[i];
+    flag = undolog.is_deleted_;
+    //  if (undolog.is_deleted_) {
+    // new_values.clear();
+    //  } else {
+    if (!flag) {
+      auto undolog_schema = GetUndoLogSchema(new_schema, undolog);
+      uint32_t count = 0;
+      for (uint32_t i = 0; i < new_schema->GetColumnCount(); ++i) {
+        if (undolog.modified_fields_[i]) {
+          new_values.at(i) = undolog.tuple_.GetValue(&undolog_schema, count);
+          ++count;
+        }
+      }
+    }
+  }
+  std::optional<Tuple> new_tuple;
+  // if (new_values.empty()) {
+  if (flag) {
+    return std::nullopt;
+  }
+  new_tuple = std::make_optional<Tuple>({new_values, new_schema});
+  return new_tuple;
 }
 
 void TxnMgrDbg(const std::string &info, TransactionManager *txn_mgr, const TableInfo *table_info,
@@ -39,6 +73,17 @@ void TxnMgrDbg(const std::string &info, TransactionManager *txn_mgr, const Table
   // RID=0/3 ts=txn6 <del marker> tuple=(<NULL>, <NULL>, <NULL>)
   //   txn6@0 (6, <NULL>, <NULL>) ts=2
   //   txn3@1 (7, _, _) ts=1
+}
+
+auto GetUndoLogSchema(const Schema *schema, const UndoLog &undo_log) -> bustub::Schema {
+  auto size = schema->GetColumnCount();
+  std::vector<uint32_t> attrs{};
+  for (uint32_t i = 0; i < size; ++i) {
+    if (undo_log.modified_fields_[i]) {
+      attrs.push_back(i);
+    }
+  }
+  return Schema::CopySchema(schema, attrs);
 }
 
 }  // namespace bustub
